6e200b052e4267ea6368e5e1a65ed165aedf1c79
zelenilo4751 <jum.jum.ju@gmail.com>
2025-01-12 22:15:58 +0800

    Update homework

---
+++ Assets/Scripts/Exercise_1_Variables.cs
@@ -0,0 +1,10 @@
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+
+public class Exercise_1_Variables : MonoBehaviour
+{
+    public int price = 999;
+    private float jumpHeight = 77.5f;
+    public string weapon = "三項之力";
+    private bool dragonSoul = true;
+}
---
+++ Assets/Scripts/Exercise_2_DataType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Exercise_2_DataType : MonoBehaviour
+{
+    public float a = 7.7f;
+    public double b = 0.12345;
+    public decimal c = 1.23456789m;
+}
