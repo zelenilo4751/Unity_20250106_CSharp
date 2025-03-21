0e6fc11916ba35931723fafa36f25c9b831abd8e
zelenilo4751 <jum.jum.ju@gmail.com>
2025-03-21 19:01:24 +0800

    Update 認識類別與建構子

---
+++ Assets/Scripts/Class_8_1_Class.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace ROKU
+{
+    /// <summary>
+    /// 類別 Class：用來定義一個物件以及他所擁有的資料與功能 (成員)
+    /// </summary>
+    public class Class_8_1_Class:MonoBehaviour
+    {
+        private void Awake()
+        {
+            // 實例化 NPC 並儲存在 npcJack 變數內
+            Class_8_1_NPC npcJack = new Class_8_1_NPC();
+            Class_8_1_NPC npcKID = new Class_8_1_NPC("KID");
+
+            npcJack.LogName();
+            npcKID.LogName();
+        }
+    }
+
+}
