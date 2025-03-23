6872100d1cb4790dd7e39bef7302f68e3a173116
zelenilo4751 <jum.jum.ju@gmail.com>
2025-03-23 08:19:27 +0800

    Update 認識裝拆箱2

--- Assets/Scripts/Class_9_1_Boxing.cs
+++ Assets/Scripts/Class_9_1_Boxing.cs
@@ -13,16 +13,26 @@ namespace ROKU
         // 實值型別的資料會儲存在 Stack 記憶體內
         private int number = 100;
 
+        // 參考型別的資料類型
+        // 物件、類別、字串
         // 物件資料類型為參考型別
         private object box;
+
+        private object boxnumber = 50;
+        private int count;
 
         private void Awake()
         {
             // 裝箱
             // 將實值型別資料放到參考型別資料內
             box = number;
-
             LogSystem.LogWithColor(box.ToString(), "#f33");
+
+            // 拆箱
+            // 將參考型別資料到實值型別資料內
+            // 在前方添加 (要轉換的資料類型)
+            count = (int)boxnumber;
+            LogSystem.LogWithColor(count.ToString(), "#f33");
         }
     }
 }
