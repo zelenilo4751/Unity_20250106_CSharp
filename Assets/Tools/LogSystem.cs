039783bdf7df15bf96e37815112a14e7fb9ae4d5
zelenilo4751 <jum.jum.ju@gmail.com>
2025-03-23 08:43:14 +0800

    Update 資料類型轉換

--- Assets/Tools/LogSystem.cs
+++ Assets/Tools/LogSystem.cs
@@ -19,6 +19,19 @@ namespace ROKU.Tools
             Debug.Log(result);
             return result;
         }
+
+        /// <summary>
+        /// 輸出訊息並指定顏色
+        /// </summary>
+        /// <param name="message">訊息</param>
+        /// <param name="color">顏色</param>
+        /// <returns>包含顏色的訊息</returns>
+        public static string LogWithColor(object message, string color)
+        {
+            string result = $"<color={color}>{message}</color>";
+            Debug.Log(result);
+            return result;
+        }
     }
 
 }
